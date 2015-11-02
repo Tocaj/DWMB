f72e01926f5e75a1293651a413e09e4c3d7852a9
frenorell <f_norell@hotmail.com>
2015-11-02 12:57:43 +0100

    Addat och deletat men hel massa fragments och activities

--- app/src/main/java/com/tocaj/martin/dwmb2/MainActivity.java
+++ app/src/main/java/com/tocaj/martin/dwmb2/MainActivity.java
@@ -1,19 +1,24 @@
 package com.tocaj.martin.dwmb2;
 
 import android.app.Activity;
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-import com.tocaj.martin.dwmb2.uiFragmentMap.SecondScreenFragment;
+import com.tocaj.martin.dwmb2.uiFragmentMap.MapFragment;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener{
 
 
-    private SecondScreenFragment secondScreenFragment;
+    private MapFragment mapFragment;
     private Button btnButton;
+    private Spinner spinner;
+
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +26,32 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         initializeComponents();
-        initializeFragment();
+     //   initializeFragment();
     }
 
     private void initializeComponents() {
         btnButton = (Button) findViewById(R.id.btnButton);
+        spinner = (Spinner) findViewById(R.id.spSpinner);
+        adapter = ArrayAdapter.createFromResource
+                (this, R.array.object_array, android.R.layout.simple_spinner_item);
+
+        spinner.setAdapter(adapter);
+        //  spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items));
+
+        spinner.setOnItemSelectedListener(this);
 
         btnButton.setOnClickListener(new StartListener());
 
+
     }
 
-    public void initializeFragment(){
-        FragmentManager fm = getFragmentManager();
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        SecondScreenFragment fragment = (SecondScreenFragment)fm.findFragmentById(R.id.fragment_second_screen);
+    }
 
+    public void onNothingSelected(AdapterView<?> parent) {
 
-
-}
+    }
 
 
 
