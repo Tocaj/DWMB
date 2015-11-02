61c6284924a83f191a4905748a7cceed34d2b559
frenorell <f_norell@hotmail.com>
2015-11-02 12:39:51 +0100

    Jobbar med spinner

--- app/src/main/java/com/tocaj/martin/dwmb2/uiFragmentMap/SliderFragment.java
+++ app/src/main/java/com/tocaj/martin/dwmb2/uiFragmentMap/SliderFragment.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.tocaj.martin.dwmb2.R;
 
@@ -13,8 +17,14 @@ import com.tocaj.martin.dwmb2.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SliderFragment extends Fragment {
+public class SliderFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
+    private ArrayAdapter adapter;
+    private Spinner spinner;
+    private SecondScreenFragment secondScreenFragment;
+    private SecondScreenActivity secondScreenActivity;
+
+   // private String[]items = new String[] {"1","5","10"}; //Alternativ till ändring i String-list
 
     public SliderFragment() {
         // Required empty public constructor
@@ -25,8 +35,46 @@ public class SliderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slider, container, false);
+        View view = inflater.inflate(R.layout.fragment_slider, container, false);
+
+        initializeComponents(view);
+
+        return view;
+    }
+
+    public void initializeComponents(View view) {
+
+        spinner = (Spinner) view.findViewById(R.id.spSpinner);
+        adapter = ArrayAdapter.createFromResource
+                (this.getActivity().getApplicationContext(), R.array.object_array, android.R.layout.simple_spinner_item);
+
+        // adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        //  spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items));
+
+        spinner.setOnItemSelectedListener(this);
+
+
+
+        //     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        //         Toast.makeText(secondScreenActivity.getApplicationContext(), "Avstånd valt: " + position, Toast.LENGTH_LONG).show();
+
+        //    }
+
+        //   }
+
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
